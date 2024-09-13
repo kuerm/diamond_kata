d87630482ef9e0ca2793c4053d36eb95940a4765
Kuerm <kuerm@gmx.net>
2024-09-13 16:17:11 +0200

    remove unused (temporary) tests

--- src/test/java/ch/css/m3000/DiamondKataIT.java
+++ src/test/java/ch/css/m3000/DiamondKataIT.java
@@ -58,49 +58,4 @@ class DiamondKataIT {
                   B B
                    A""");
     }
-
-    @Test
-    void getIndentationWhenTwoCharactersThenReturnFirstCharacterWith0AndSecondWith1() {
-        IndentationChar[] actual = sut.calculateIndentation(new char[]{'*', 's'});
-
-        assertThat(actual)
-                .containsExactly(new IndentationChar('*', 1), new IndentationChar('s', 0));
-    }
-
-    @Test
-    void getIndentationWhenOnlyOneCharacter() {
-        IndentationChar[] actual = sut.calculateIndentation(new char[]{'*'});
-
-        assertThat(actual)
-                .containsExactly(new IndentationChar('*', 0));
-    }
-
-    @Test
-    void getStringWhenOnlyOneCharacter() {
-        String actual = sut.getString(new IndentationChar[]{new IndentationChar('*', 0)}, 0);
-
-        assertThat(actual)
-                .isEqualTo("*");
-    }
-
-    @Test
-    void getStringWhenTwoCharactersAndOneINdentation() {
-        String actual = sut.getString(new IndentationChar[]{new IndentationChar('*', 1), new IndentationChar('s', 0)}, 1);
-
-        assertThat(actual)
-                .isEqualTo("""
-                         *
-                        s s
-                         *""");
-    }
-
-    @Test
-    void getIndentationWhenThreeCharacters() {
-        IndentationChar[] actual = sut.calculateIndentation(new char[]{'*', 's', 'd'});
-
-        assertThat(actual)
-                .containsExactly(new IndentationChar('*', 2), new IndentationChar('s', 1), new IndentationChar('d', 0));
-    }
-
-
 }
