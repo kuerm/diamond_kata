c2385c827564dc95f276e0d60f0384b5e4522b6c
Kuerm <kuerm@gmx.net>
2024-09-13 16:03:29 +0200

    implemented toString

--- src/test/java/ch/css/m3000/DiamondKataIT.java
+++ src/test/java/ch/css/m3000/DiamondKataIT.java
@@ -5,15 +5,16 @@ import org.junit.jupiter.api.Test;
 import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class DiamondKataIT {
     DiamondKata sut = new DiamondKata();
 
     @Test
     void diamondWhenWrongInputSeedThenReturnInvalidInputAsString() {
-        String actual = sut.diamond('0');
+        char invalidInput = '0';
 
-        assertThat("Invalid Input").isEqualTo(actual);
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> sut.diamond(invalidInput));
     }
 
     @Test
@@ -25,36 +26,44 @@ class DiamondKataIT {
 
     @Test
     void diamondWhenInputSeedIsLowerCaseBThenReturnABBA() {
-
         String actual = sut.diamond('b');
 
-        assertThat("""
+        assertThat(actual).isEqualTo("""
                  A
                 B B
-                 A""")
-                .isEqualTo(actual);
+                 A""");
     }
 
     @Test
+    void diamondWhenInputSeedIsCThenReturnDiamond() {
+        String actual = sut.diamond('C');
+
+        assertThat(actual).isEqualTo("""
+                  A
+                 B B
+                C   C
+                 B B
+                  A""");
+    }
+
+
+    @Test
     void diamondWhenInputSeedIsDThenReturnDiamond() {
-
-
         String actual = sut.diamond('D');
 
-        assertThat("""
+        assertThat(actual).isEqualTo("""
                    A
                   B B
                  C   C
                 D     D
                  C   C
                   B B
-                   A""")
-                .isEqualTo(actual);
+                   A""");
     }
 
     @Test
     void getIndentationWhenTwoCharactersThenReturnFirstCharacterWith0AndSecondWith1() {
-        IndentationChar[] actual = sut.getIndentation(new char[]{'*', 's'});
+        IndentationChar[] actual = sut.calculateIndentation(new char[]{'*', 's'});
 
         assertThat(actual)
                 .containsExactly(new IndentationChar('*', 1), new IndentationChar('s', 0));
@@ -62,15 +71,34 @@ class DiamondKataIT {
 
     @Test
     void getIndentationWhenOnlyOneCharacter() {
-        IndentationChar[] actual = sut.getIndentation(new char[]{'*'});
+        IndentationChar[] actual = sut.calculateIndentation(new char[]{'*'});
 
         assertThat(actual)
                 .containsExactly(new IndentationChar('*', 0));
     }
 
     @Test
+    void getStringWhenOnlyOneCharacter() {
+        String actual = sut.getString(new IndentationChar[]{new IndentationChar('*', 0)}, 0);
+
+        assertThat(actual)
+                .isEqualTo("*");
+    }
+
+    @Test
+    void getStringWhenTwoCharactersAndOneINdentation() {
+        String actual = sut.getString(new IndentationChar[]{new IndentationChar('*', 1), new IndentationChar('s', 0)}, 1);
+
+        assertThat(actual)
+                .isEqualTo("""
+                         *
+                        s s
+                         *""");
+    }
+
+    @Test
     void getIndentationWhenThreeCharacters() {
-        IndentationChar[] actual = sut.getIndentation(new char[]{'*', 's', 'd'});
+        IndentationChar[] actual = sut.calculateIndentation(new char[]{'*', 's', 'd'});
 
         assertThat(actual)
                 .containsExactly(new IndentationChar('*', 2), new IndentationChar('s', 1), new IndentationChar('d', 0));
@@ -94,7 +122,53 @@ class DiamondKataIT {
 
     class DiamondKata {
 
+        private static String getLowerPartString(IndentationChar[] indentationChars, int maxIndentation) {
+            StringBuilder result = new StringBuilder();
+            for (int i = indentationChars.length - 2; i >= 0; i--) {
+                IndentationChar indentationChar = indentationChars[i];
+                result.append(" ".repeat(indentationChar.indentation))
+                        .append(indentationChar.character)
+                ;
+                if (indentationChar.indentation != maxIndentation) {
+                    result.append(" ".repeat(2 * maxIndentation - 2 * indentationChar.indentation - 1))
+                            .append(indentationChar.character);
+                }
+                result.append("\n");
+            }
+            return result.toString();
+        }
+
+        private static String getUpperPartString(IndentationChar[] indentationChars, int maxIndentation) {
+            StringBuilder result = new StringBuilder();
+            for (IndentationChar indentationChar : indentationChars) {
+                result.append(" ".repeat(indentationChar.indentation))
+                        .append(indentationChar.character);
+                if (indentationChar.indentation != maxIndentation) {
+                    result.append(" ".repeat(2 * maxIndentation - 2 * indentationChar.indentation - 1))
+                            .append(indentationChar.character);
+                }
+                result.append("\n");
+            }
+            return result.toString();
+        }
+
+        private static int calculateMaxIndentation(IndentationChar[] indentationChars) {
+            return indentationChars.length - 1;
+        }
+
+        private static String concat(String firstPart, String secondPart) {
+            return firstPart + secondPart;
+        }
+
+        private static String removeLastNewLine(String fullDiamond) {
+            return fullDiamond.substring(0, fullDiamond.length() - 1);
+        }
+
         private char validate(char seed) {
+            // if seed is not a letter
+            if (!Character.isLetter(seed)) {
+                throw new IllegalArgumentException("Invalid Input");
+            }
             return Character.toUpperCase(seed);
         }
 
@@ -106,15 +180,14 @@ class DiamondKataIT {
             return characters;
         }
 
-        private String getString(IndentationChar[] indentationChars) {
-            StringBuilder returnvalue = new StringBuilder();
-            for (IndentationChar indentationChar : indentationChars) {
-                returnvalue.append(indentationChar.character);
-            }
-            return returnvalue.toString();
+        String getString(IndentationChar[] indentationChars, int maxIndentation) {
+            String firstPart = getUpperPartString(indentationChars, maxIndentation);
+            String secondPart = getLowerPartString(indentationChars, maxIndentation);
+            String fullDiamond = concat(firstPart, secondPart);
+            return removeLastNewLine(fullDiamond);
         }
 
-        IndentationChar[] getIndentation(char[] characters) {
+        IndentationChar[] calculateIndentation(char[] characters) {
             int totalCharacters = characters.length;
             IndentationChar[] indentationChars = new IndentationChar[totalCharacters];
 
@@ -129,9 +202,9 @@ class DiamondKataIT {
             char validatedSeed = validate(seed);
             char[] characters = detectCharacters(validatedSeed);
             // detect on how many characters has to be displayed how much indentation every one has
-            IndentationChar[] indentationChars = getIndentation(characters);
-            // display
-            String diamond = getString(indentationChars);
+            IndentationChar[] indentationChars = calculateIndentation(characters);
+            int maxIndentation = calculateMaxIndentation(indentationChars);
+            String diamond = getString(indentationChars, maxIndentation);
             return diamond;
         }
 
